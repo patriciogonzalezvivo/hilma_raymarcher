5f589f0d291f8d90dfc066a282f85ca6e6aa6017
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2023-01-21 20:58:37 -0500

    better comments

--- src/main.cu
+++ src/main.cu
@@ -12,8 +12,7 @@
 #include "hilma/types/Image.h"
 #include "hilma/io/png.h"
 
-__global__ void render(int _width, int _height, float *_pixels)
-{
+__global__ void render(int _width, int _height, float *_pixels) {
     int x = blockIdx.x * blockDim.x + threadIdx.x;
     int y = blockIdx.y * blockDim.y + threadIdx.y;
     if (x >= _width || y >= _height)
@@ -21,8 +20,7 @@ __global__ void render(int _width, int _height, float *_pixels)
 
     float4 color = make_float4(0.0f, 0.0f, 0.0f, 1.0f);
     float2 pixel = make_float2(1.0 / _width, 1.0 / _height);
-    float2 st = make_float2(x, y);
-    st = st * pixel;
+    float2 st = make_float2(x, y) * pixel;
 
     color.x = st.x;
     color.y = st.y;
@@ -34,29 +32,28 @@ __global__ void render(int _width, int _height, float *_pixels)
     _pixels[x * 4 + 4 * y * _width + 3] = color.w;
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     int width = 1920;
     int height = 1080;
 
-    hilma::Image image = hilma::Image(width, height, 4);
+    // Create a buffer of pixels to be process on __device__
+    float *d_pixels;
+    cudaMalloc(&d_pixels, 4 * width * height * sizeof(float));
 
+    // Hand the pixels to a kernel to be process on __device__ 
     dim3 threads(BLOCK_SIZE, BLOCK_SIZE);
     dim3 blocks(width / threads.x + 1, height / threads.y + 1);
+    render<<<blocks, threads>>>(width, height, d_pixels);
 
-    float *device_pixels;
-    cudaMalloc(&device_pixels, 4 * width * height * sizeof(float));
+    // Copy pixels processed on __device__ into a HILMA Image on __host__
+    hilma::Image image = hilma::Image(width, height, 4);
+    cudaMemcpy(&image[0], d_pixels, 4 * width * height * sizeof(float), cudaMemcpyDeviceToHost);
 
-    render<<<blocks, threads>>>(width, height, device_pixels);
-
-    float *host_pixels = (float*)malloc(4 * width * height * sizeof(float));
-    cudaMemcpy(host_pixels, device_pixels, 4 * width * height * sizeof(float), cudaMemcpyDeviceToHost);
-
-    image.setColors(host_pixels, width * height, 4);
+    // Save image
     hilma::savePng("image.png", image);
 
-    cudaFree(device_pixels);
-    free(host_pixels);
+    // Free pixels on __device__
+    cudaFree(d_pixels);
 
     return 0;
 }
